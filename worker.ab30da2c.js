fa89ccede95f8fec9febec78bfb88bd9c282b45c
manhnvm <manhnvworkspace@gmail.com>
2021-10-27 02:24:50 +0700

    Updates

--- worker.ab30da2c.js
+++ worker.ab30da2c.js
@@ -7648,7 +7648,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58771" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64745" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
